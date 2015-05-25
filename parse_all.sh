2ace566919d61988d9b777afa488ec9d6be86aa3
Matthias Wahl <matthias@crate.io>
2015-05-25 09:47:08 +0200

    delete old parse_all.sh

--- parse_all.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-for f in $`ls downloads/temperature | awk  -F '_' '{sub(/-.*$/, "", $2); print $2}'`
-do 
-    bin/parse_data -s $f --temp-download-dir downloads/temperature --sunshine-download-dir downloads/sunshine_duration --wind-download-dir downloads/wind --rainfall-download-dir downloads/rainfall --earth-download-dir downloads/earth_ground
-done
